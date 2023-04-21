ec3d3f215259456aa131d84c75719d08434a64e5
abductedRhino <avocadorable@mailbox.org>
2023-04-21 19:01:31 +0200

    add get_md5()

--- part3.py
+++ part3.py
@@ -1,8 +1,10 @@
 import glob
+import hashlib
 # https://developer.mozilla.org/
 
 globs = glob.iglob("./Downloads/**",
                    recursive=True)
+
 
 
 def print_globs():
@@ -11,10 +13,18 @@ def print_globs():
 
 
 # https://docs.python.org/3/library/functions.html#open
-def print_file(path):
-    print()
+# https://www.youtube.com/watch?v=FxjeV0ePOKU
+def get_md5(path):
+    hasher = hashlib.md5()
+    with open(path, 'rb') as file:
+        data = file.read()
+        hasher.update(data)
+    return hasher.hexdigest()
 
 
 if __name__ == '__main__':
     print_globs()
+    print(get_md5('./Downloads/kursliste.txt'))
+    print(get_md5('./Downloads/kursliste(1).txt'))
+    print(get_md5('./Downloads/kursliste(2).txt'))
 
